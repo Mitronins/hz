ad04eb309515acc4680d393401117b7e3d16c9a1
Mitronin <mitronin.sergei@gmail.com>
2018-05-13 20:51:00 +0300

    add right answers to tests

--- src/components/ListTests/OneTest/index.jsx
+++ src/components/ListTests/OneTest/index.jsx
@@ -4,6 +4,7 @@ import styles from './styles.scss';
 import {Link} from "react-router-dom";
 
 
+
 class OneTest extends Component {
 
     render() {
@@ -12,6 +13,7 @@ class OneTest extends Component {
             <Link to={`/test/${test.id}`} className={styles.container}>
                 <div className={styles.title}>{test.title}</div>
                 <div className={styles.description}>{test.description}</div>
+                {test.status === 1 && <div className={styles['right-answer']}>Правильные ответы: {test.right_answers}</div>}
                 {test.status === 0 && <div className={styles.clock}/>}
                 {test.status === null && <div className={styles.spread}/>}
                 {test.status === 1 && <div className={styles.check}/>}
